d3b37d4eeddaf5733d3a7826961eacabfa7f16fe
RVH <rvhrakotonaina@gmail.com>
2026-09-12 19:49:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const cors = require('cors');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static('.'));
